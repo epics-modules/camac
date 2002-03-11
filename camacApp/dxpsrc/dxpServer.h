15b9c85844817bbaa4f4a307548f2a028590a6c0
Mark Rivers <MarkRivers@users.noreply.github.com>
2002-03-11 03:26:29 +0000

    Added support for FIPPI field

--- camacApp/dxpsrc/dxpServer.h
+++ camacApp/dxpsrc/dxpServer.h
@@ -7,6 +7,7 @@
 #define MSG_DXP_SET_LONG_PARAM           101
 #define MSG_DXP_CALIBRATE                102
 #define MSG_DXP_READ_PARAMS              103
+#define MSG_DXP_DOWNLOAD_FIPPI           104
 
 #define MODEL_DXP4C 0
 #define MODEL_DXP2X 1
